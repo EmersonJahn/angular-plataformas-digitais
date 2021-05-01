53885e8da34190b482cefa5fffa1a3c712dcf2de
Emerson Jahn <emersonajahn@gmail.com>
2021-05-01 10:49:00 -0300

    create problem-registration component.

---
+++ src/app/problem-registration/problem-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-problem-registration',
+  templateUrl: './problem-registration.component.html',
+  styleUrls: ['./problem-registration.component.css']
+})
+export class ProblemRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
