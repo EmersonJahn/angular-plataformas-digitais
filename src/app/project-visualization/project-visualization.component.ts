0dc882ca2312020a8cdb7131fbf9e1ce482ec0f8
Emerson Jahn <emersonajahn@gmail.com>
2021-05-01 11:06:52 -0300

    create the project-visualization component.

---
+++ src/app/project-visualization/project-visualization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-visualization',
+  templateUrl: './project-visualization.component.html',
+  styleUrls: ['./project-visualization.component.css']
+})
+export class ProjectVisualizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
