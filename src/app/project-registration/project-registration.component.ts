ace31a7cf692c93025119c52c49cea88ce0e0e8b
Emerson Jahn <emersonajahn@gmail.com>
2021-05-01 10:49:28 -0300

    create project-registration component.

---
+++ src/app/project-registration/project-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-registration',
+  templateUrl: './project-registration.component.html',
+  styleUrls: ['./project-registration.component.css']
+})
+export class ProjectRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
