80a924017450457273e75336da721d8741e869bf
Emerson Jahn <emersonajahn@gmail.com>
2021-05-15 09:43:48 -0300

    implemented provisional getCategories

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,4 +10,13 @@ export class AppService {
 
     constructor () {}
 
+    getCategories() {
+        const categories: Category[] = [];
+        for (let index = 1; index < 4; index++) {
+            const category = new Category(index, "Categoria " + index)
+            categories.push(category);
+        }
+        return categories;
+    }
+
 }
