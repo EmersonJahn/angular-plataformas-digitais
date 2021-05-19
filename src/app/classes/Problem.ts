fc30e115e43ddb94b1d4791668cca6410e0b2ff6
Emerson Jahn <emersonajahn@gmail.com>
2021-05-19 10:52:42 -0300

    front-end problems component implemented.

--- src/app/classes/Problem.ts
+++ src/app/classes/Problem.ts
@@ -6,14 +6,16 @@ export class Problem {
     public title: string;
     public description: string;
     public problem_status_id: Number;
+    public number_answers: Number;
 
-    constructor (id: Number, personId: Number, categoryId: Number, title: string, description: string, problemStatusId: Number,) {
+    constructor (id: Number, personId: Number, categoryId: Number, title: string, description: string, problemStatusId: Number, numberAnswers: Number) {
         this.id                = id;
         this.person_id         = personId;
         this.category_id       = categoryId;
         this.title             = title;
         this.description       = description;
         this.problem_status_id = problemStatusId;
+        this.number_answers    = numberAnswers;
     }
 
 }
--- src/app/problems/problems.component.ts
+++ src/app/problems/problems.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AppService } from '../app.service';
+import { Category } from '../classes/Category';
+import { Problem } from '../classes/Problem';
 
 @Component({
   selector: 'app-problems',
   templateUrl: './problems.component.html',
-  styleUrls: ['./problems.component.css']
+  styleUrls: ['./problems.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProblemsComponent implements OnInit {
 
-  constructor() { }
+  public category: any; 
+  public categories: Category[] = [];
+  public problems: Problem[]    = [];
+
+  // public dropdownSettings = {};
+
+  constructor(private appService: AppService) { }
 
   ngOnInit(): void {
+    // this.setDropdownSettings();
+    this.getCategories();
+    this.getProblems();
+
+    localStorage.setItem("selectedProblemId", "0");
+  }
+
+  public getProblems() {
+    this.problems = [];
+    for (let index = 1; index < 6; index++) {
+      const problem = new Problem(index, index, index, "Título problema - " + index, "Descrição problema - " + index + ": Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse tenetur ratione vero laudantium quidem alias officiis recusandae! Error, assumenda soluta. Velit labore blanditiis necessitatibus voluptas, fugiat ex aspernatur vel architecto.", index, index);
+      this.problems.push(problem);
+    }
+
+    // TODO validar filtros
+    // TODO buscar no banco
+  }
+
+  public selectProblem(problem: Problem) {
+    localStorage.setItem("selectedProblemId", problem.id.toString());
+  }
+
+  // private setDropdownSettings() {
+  //   this.dropdownSettings = {
+  //     singleSelection: true,
+  //     idField: 'id',
+  //     textField: 'description',
+  //     allowSearchFilter: true,
+  //     searchPlaceholderText: 'BUSCAR',
+  //     noDataAvailablePlaceholderText: "ERRO AO CARREGAR AS CATEGORIAS",
+  //   };
+  // }
+
+  private getCategories() {
+    this.categories = this.appService.getCategories();
   }
 
 }
