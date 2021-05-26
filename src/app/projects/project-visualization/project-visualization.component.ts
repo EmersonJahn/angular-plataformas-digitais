5ec0a4a686be83192491950c4e50d1c1d35d4557
Emerson Jahn <emersonajahn@gmail.com>
2021-05-26 13:00:40 -0300

    continue front-end project-visualization component implementation.

--- src/app/projects/project-visualization/project-visualization.component.ts
+++ src/app/projects/project-visualization/project-visualization.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Project } from 'src/app/classes/Project';
+import { ProjectMember } from './../../classes/ProjectMember';
 
 @Component({
   selector: 'app-project-visualization',
@@ -11,7 +13,8 @@ export class ProjectVisualizationComponent implements OnInit {
 
   public selectedProjectId = 0;
 
-  // public project: Project;
+  public project?: Project;
+  public projectMembers: ProjectMember[] = [];
 
   constructor(private route: ActivatedRoute) { }
 
@@ -22,7 +25,12 @@ export class ProjectVisualizationComponent implements OnInit {
   }
 
   private getProject() {
-    // this.project = new Project(this.selectedProjectId, this.selectedProjectId, "Projeto - " + this.selectedProjectId, "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, dolore, nesciunt nihil magnam esse vitae explicabo similique earum praesentium iure excepturi commodi itaque quia in accusamus natus dolorem quam debitis.", "assets/images/project-icon.png");
+    this.project = new Project(this.selectedProjectId, this.selectedProjectId, "Projeto - " + this.selectedProjectId, "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, dolore, nesciunt nihil magnam esse vitae explicabo similique earum praesentium iure excepturi commodi itaque quia in accusamus natus dolorem quam debitis.", "assets/images/project-icon.png");
+    this.getProjectMembers();
+  }
+
+  private getProjectMembers() {
+
   }
 
 }
