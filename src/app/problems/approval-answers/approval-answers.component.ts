9630e35677636c81453e2aaa66667f6b8ef2b388
Emerson Jahn <emersonajahn@gmail.com>
2021-05-31 12:57:23 -0300

    started front-end approval-answers.component component implementation.

--- src/app/problems/approval-answers/approval-answers.component.ts
+++ src/app/problems/approval-answers/approval-answers.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import { PendingAnswer } from './../../classes/PendingAnswer';
 
 @Component({
   selector: 'app-approval-answers',
   templateUrl: './approval-answers.component.html',
-  styleUrls: ['./approval-answers.component.css']
+  styleUrls: ['./approval-answers.component.css', '../problems.component.css']
 })
 export class ApprovalAnswersComponent implements OnInit {
+
+  public faCheck = faCheck;
+  public faTimes = faTimes;
+
+  public pendingAnswers: PendingAnswer[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getPendingAnswers();
+  }
+
+  private getPendingAnswers() {
+    for (let index = 1; index < 6; index++) {
+      const pendingAnswer =  new PendingAnswer(index, index);
+      this.pendingAnswers.push(pendingAnswer); 
+    }
   }
 
 }
