24c39b9471a9c1f86fa8bb98d54d429f1254b3cf
Emerson Jahn <emersonajahn@gmail.com>
2021-06-16 14:34:40 -0300

    back-end project-registration component implemented.

--- src/app/classes/Project.ts
+++ src/app/classes/Project.ts
@@ -4,14 +4,14 @@ export class Project {
     public category_id: Number;
     public title: string;
     public description: string;
-    public project_image: string;
+    public project_photo: string;
 
-    constructor (id: Number, categoryId: Number, title: string, description: string, projectImage: string) {
+    constructor (id: Number, categoryId: Number, title: string, description: string, projectPhoto: string) {
         this.id            = id;
         this.category_id   = categoryId;
         this.title         = title;
         this.description   = description;
-        this.project_image = projectImage;
+        this.project_photo = projectPhoto;
     }
     
 }
