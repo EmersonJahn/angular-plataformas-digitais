dd903b83a7dd4efee63eb1a6255b2b00ca1fa3c9
Emerson Jahn <emersonajahn@gmail.com>
2021-06-23 14:58:06 -0300

    some adjusts in classes.

--- src/app/classes/Project.ts
+++ src/app/classes/Project.ts
@@ -1,16 +1,19 @@
+import { Person } from 'src/app/classes/Person';
+import { Category } from 'src/app/classes/Category';
+
 export class Project {
 
     public id: Number;
-    public person_id: Number;
-    public category_id: Number;
+    public person: Person;
+    public category: Category;
     public title: string;
     public description: string;
     public project_photo: string;
 
-    constructor (id: Number, personId:Number, categoryId: Number, title: string, description: string, projectPhoto: string) {
+    constructor (id: Number, person:Person, category: Category, title: string, description: string, projectPhoto: string) {
         this.id            = id;
-        this.person_id     = personId;
-        this.category_id   = categoryId;
+        this.person        = person;
+        this.category      = category;
         this.title         = title;
         this.description   = description;
         this.project_photo = projectPhoto;
